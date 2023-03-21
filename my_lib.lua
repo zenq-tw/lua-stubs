82884b18fb6b14e1299c27661ad69e28e67c140f
zenq <zenq.tw@gmail.com>
2023-03-22 00:56:08 +0400

    common.get_context_value

--- my_lib.lua
+++ my_lib.lua
@@ -1,1 +1,8 @@
 core.load_global_script = cm.load_global_script
+
+---Will look up the context object from the context cache or construct one with the type "context_object_typename" and the data (usually a CQI) from "construction_data", call the context function specified by context_function_id and return the value.
+---@param class? string #optional, default value="CcoScriptObject" Context object id on which to call the function. If omitted, the function is called on the CcoScriptObject object.
+---@param object_id string | integer some identificator (FIXME: is number is ok?)
+---@param func string  usage "method_name()" #Function id on the context object to call. This can also be an expression, but the result must return a context id.
+---@return ComponentContextObject | any
+function common.get_context_value(class, object_id, func) end
