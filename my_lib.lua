ece1dad07cd6303df817c6016c153ba8cb2a353e
zenq <zenq.tw@gmail.com>
2023-03-22 20:45:00 +0400

    remove&apply effect_bundle_from[_characters]_force

--- my_lib.lua
+++ my_lib.lua
@@ -38,3 +38,29 @@ function cm:spawn_plague_at_settlement(faction, settlement, plague_key) end
 ---@param military_force MILITARY_FORCE_SCRIPT_INTERFACE #Target military force.
 ---@param plague_key string #Plague key, from the plagues table.
 function cm:spawn_plague_at_military_force(faction, military_force, plague_key) end
+
+
+--- Applies an effect bundle to a military force by cqi for a number of turns (can be infinite).
+---@param effect_bundle_key string #Effect bundle key from the effect bundles table.
+---@param number_cqi integer #Command queue index of the military force to apply the effect bundle to.
+---@param turns number #Number of turns to apply the effect bundle for. Supply 0 here to apply the effect bundle indefinitely (it can be removed later with campaign_manager:remove_effect_bundle_from_force if required).
+function cm:apply_effect_bundle_to_force(effect_bundle_key, number_cqi, turns) end
+
+
+--- Removes an effect bundle from a military force by cqi.
+---@param effect_bundle_key string #Effect bundle key from the effect bundles table.
+---@param number_cqi integer #Command queue index of the military force to remove the effect from.
+function cm:remove_effect_bundle_from_force(effect_bundle_key, number_cqi) end
+
+
+--- This function applies an effect bundle to a military force for a number of turns (can be infinite). It differs from campaign_manager:apply_effect_bundle_to_force by referring to the force by its commanding character's cqi.
+---@param effect_bundle_key string #Effect bundle key from the effect bundles table.
+---@param number_cqi integer #Command queue index of the military force's commanding character to apply the effect bundle to.
+---@param turns number #Number of turns to apply the effect bundle for. Supply 0 here to apply the effect bundle indefinitely (it can be removed later with campaign_manager:remove_effect_bundle_from_characters_force if required).
+function cm:apply_effect_bundle_to_characters_force(effect_bundle_key, number_cqi, turns) end
+
+
+--- Removes an effect bundle from a military force by its commanding character's cqi.
+---@param effect_bundle_key string #Effect bundle key from the effect bundles table.
+---@param number_cqi integer #Command queue index of the character commander of the military force to remove the effect from.
+function cm:remove_effect_bundle_from_characters_force(effect_bundle_key, number_cqi) end
